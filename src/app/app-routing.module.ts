2593b3b23fe3647e4be5f358b7eae5f8726148b3
Jonathan Torres <exodiakah@gmail.com>
2022-07-23 07:52:59 -0400

    Fixed Redirect

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes, PreloadAllModules } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  {
-    path: 'dashboard',
-    loadChildren: () => import('./components/home/home.module').then(m => m.HomeModule)
-  },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
   {
     path: 'login',
     loadChildren: () => import('./components/login/login.module').then(m => m.LoginModule)
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () => import('./components/home/home.module').then(m => m.HomeModule)
   },
   { path: '**', redirectTo: '/login', pathMatch: 'full' }
 ];
